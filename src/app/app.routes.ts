729ba6a494995280d81bc554d8d21fa3b340b54d
Gabriel Nunes <gabriel.microsmp@gmail.com>
2024-08-10 01:13:59 -0300

    configure routes and providers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PersonListComponent } from './person-list/person-list.component';
+import { PersonFormComponent } from './person-form/person-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: PersonListComponent },
+  { path: "form", component: PersonFormComponent }
+];
